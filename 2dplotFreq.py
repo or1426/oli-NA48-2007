e80f36f2e8b02f35ea5813b4edb9372caf3ccd16
Oliver Reardon-Smith <or1426@bris.ac.uk>
2013-09-19 14:35:19 +0100

    updated 2dPlotter

--- 2dplotFreq.py
+++ 2dplotFreq.py
@@ -21,39 +21,31 @@ parser.add_argument('-ly',help='lower y limit',type=float,default=-sys.float_inf
 parser.add_argument('-uy',help='upper y limit',type=float,default=sys.float_info.max)
 parser.add_argument('-xL','--x-label',help='x axis label',type=str,default='')
 parser.add_argument('-yL','--y-label',help='x axis label',type=str,default='')
-
+parser.add_argument('-x',help='Indicates which parameter to use for the x axis (default is 0 (first))',type=int,default=0)
+parser.add_argument('-y',help='Indicates which parameter to use for the y axis (default is 1 (second))',type=int,default=1)
 args = parser.parse_args()
 #print args
 matplotlib.rcParams['text.latex.unicode']=True #for greek letters
-def ovalMod(coord,xMinMax,yMinMax):
-    x, y = coord
-    xc = maths.pow( (2*x - (xMinMax[0] + xMinMax[1]))/(xMinMax[1] - xMinMax[0]), 2)
-    yc = maths.pow( (2*y - (yMinMax[0] + yMinMax[1]))/(yMinMax[1] - yMinMax[0]), 2)
-    return maths.sqrt(xc + yc)
 
-def convertData(data,xLowerLimit,xUpperLimit,yLowerLimit,yUpperLimit):
-    r1,r2 = [],[]
+def convertData(data,xI,yI,xLowerLimit,xUpperLimit,yLowerLimit,yUpperLimit):
+    xl,yl = [],[]
     for thing in data:
         try:
-            s1, s2,s3,s4,s5,s6 = thing.split()
-            f1, f2 = float(s5),float(s2)
-            coord = f1,f2
-
-            if ~maths.isnan(f1) and ~maths.isnan(f2):
-               if (xLowerLimit < f1 < xUpperLimit) and (yLowerLimit < f2 < yUpperLimit):
-                   #xMinMax = (-4.89,4.89)
-                   #yMinMax = (0.469,0.521)
-                   #if ovalMod(coord,xMinMax,yMinMax) > 1 and not(f1 < 15 and f2 > 0.54):
-                   r1.append(f1)
-                   r2.append(f2)
+            strings = thing.split()
+            x = float(strings[xI])
+            y = float(strings[yI])
+            if maths.isnan(x) and maths.isnan(y):
+                raise ValueError
+            if (xLowerLimit < x < xUpperLimit) and (yLowerLimit < y < yUpperLimit):
+                xl.append(x)
+                yl.append(y)
         except ValueError:
             #print "a ValueError exeption has happened FUCKING PANIC!!!!!"
             continue
-    return r1, r2
-
+    return xl, yl
 
 for inFile in args.i:
-    convListX,convListY = convertData(inFile.readlines(),args.lx,args.ux,args.ly,args.uy) 
+    convListX,convListY = convertData(inFile.readlines(),args.x,args.y,args.lx,args.ux,args.ly,args.uy) 
     
     freqMap,xEdges,yEdges = np.histogram2d(convListX,convListY,bins=args.b)
     freqMap += 1 #this is massively dodgy but otherwise we do log(0) and fuck shit up
